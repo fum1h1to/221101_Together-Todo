f46d21306779d58f351dfc32bc6129bd66c578b8
foodcourt1 <aka464946494649@gmail.com>
2022-11-04 16:49:53 +0900

    ログインログアウト機能を追加しました。

--- account/views.py
+++ account/views.py
@@ -1,9 +1,16 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 
-from account.forms import SignupForm
+from account.forms import SignupForm,LoginForm
+from together_todo.settings import LOGIN_REDIRECT_URL
 from .models import UserActivateTokens
 from django.http import HttpResponse
+
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib.auth.models import User
 
 app_name = 'account'
 
@@ -15,16 +22,60 @@ def activate_user(request, activate_token):
             UserActivateTokens.objects.send_activate_success(activated_user)
             return render(request, 'account/activate_success.html')
     return render(request, 'account/activate_failed.html')
-
+   
 class TopView(TemplateView):
     template_name = 'account/top.html'
 
-class LoginView(TemplateView):
+
+
+
+class Login_success(LoginRequiredMixin, TemplateView):
+    template_name = 'account/login_success.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
+
+
+
+class LoginView(LoginView):
+
+  
+    form_class = LoginForm
     template_name = 'account/login.html'
+    
+
+
+
+
+class Logout_success(LogoutView):
+    
+    template_name='account/logout_success.html'
+    form_class = LoginForm
+    
+
+##追加
+class OtherView(LoginRequiredMixin, TemplateView):
+    template_name = 'login_app/other.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users'] = User.objects.exclude(username=self.request.user.username)
+        return context
+
+
+
+
+
 
 class SignupView(TemplateView):
     template_name = 'account/signup.html'
 
+     
+
+     
+    
     def get(self, request, *args, **kwargs):
         form = SignupForm()
         context = {
@@ -43,6 +94,7 @@ class SignupView(TemplateView):
             'form': form
         }
         return self.render_to_response(context)
+
 
 class ActivateSuccessView(TemplateView):
     template_name = 'account/activate_success.html'
--- together_todo/settings.py
+++ together_todo/settings.py
@@ -151,3 +151,7 @@ EMAIL_USE_TLS = True
 # auth settings
 AUTH_USER_MODEL = 'account.CustomUser'
 ACTIVATION_EXPIRED_MINUTES = 30
+
+LOGIN_URL='login'
+##ログイン成功後に行くサイトの名前
+LOGIN_REDIRECT_URL = 'login_success'    ### 追加
