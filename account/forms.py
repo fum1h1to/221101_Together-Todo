a72e39ec136b6b83c6c0a50708f0c96f2dc3a729
Fumihito Sudo <75571344+fum1h1to@users.noreply.github.com>
2022-11-06 21:15:34 +0900

    djangoにデザイン反映、コメントの追加

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
+from django.utils.translation import gettext_lazy as _
 
 from account.models import UserActivateTokens
 
@@ -26,4 +27,22 @@ class SignupForm(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
-    pass
+    username = UsernameField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "autofocus": True,
+                "class": "form-control"
+            }
+        )
+    )
+    password = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "current-password",
+                "class": "form-control"
+            }
+        ),
+    )
