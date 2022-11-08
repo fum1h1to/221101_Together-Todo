8d94d02747cbdc9f3a3cce9a2eda9dea2720ee86
Fumihito Sudo <75571344+fum1h1to@users.noreply.github.com>
2022-11-08 23:50:12 +0900

    djangoにG102,G104,G105反映

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, password_validation
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.utils.translation import gettext_lazy as _
 
@@ -9,8 +9,36 @@ from account.models import CustomUser
 User = get_user_model()
 
 class SignupForm(UserCreationForm):
-    username = forms.CharField(label= "ユーザ名")
-    email = forms.EmailField(label="メールアドレス")
+    username = forms.CharField(
+        label= _("ユーザ名"),
+        widget=forms.TextInput(
+            attrs={
+                "autofocus": True,
+                "class": "form-control"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label=_("メールアドレス"),
+        widget=forms.EmailInput(
+            attrs={
+                "autofocus": True,
+                "class": "form-control"
+            }
+        )
+    )
+    password1 = forms.CharField(
+        label=_("パスワード"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password", "class": "form-control"}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("パスワード（確認用）"),
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password", "class": "form-control"}),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
     
     class Meta:
         model = User
