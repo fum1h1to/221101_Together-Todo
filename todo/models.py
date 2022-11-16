c861da4b4bf809a2a07fe9d3a78d8885032488be
foodcourt1 <aka464946494649@gmail.com>
2022-11-16 17:36:05 +0900

    2022/11/15日に追加したタスク機能

--- todo/models.py
+++ todo/models.py
@@ -10,19 +10,30 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
+class TaskManager(models.Manager):
+    '''
+    タスクのマネージャー
+    '''
+    def create(self, user, taskName, deadline, importance, note, isBocchi, requestUsers):
+        '''
+        タスクの作成をする
+        '''
+        task = Task(userid=user,taskName=taskName,deadline=deadline,importance=importance,
+                                 note=note,isBocchi=isBocchi,)
+        task.save()
+        
+        for username in requestUsers:
+            user = CustomUser.objects.get(username=username)
+            Commission.objects.create(user, task)
 
 class Task(models.Model):
     
-
-    ##これはForm.pyに書くのかな？
     def moviefile_size(value): ##動画サイズを設定する
 
         limit=536870912 ##512MBをbyteに変換
         if value.size>limit:
-
             raise ValidationError('動画のサイズが512MBを超えています。')
 
-    
     def imagefile_size(value):##画像サイズを設定する
 
         limit=5242880##5MBをbyteに変換
@@ -30,52 +41,62 @@ class Task(models.Model):
 
             raise ValidationError('画像のサイズが5MBを超えています。')
 
-    
-
-
-
-   
     taskid=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) ##タスクID 主キー
 
     userid=models.ForeignKey(CustomUser, on_delete=models.CASCADE) ##ユーザID accountファイルからもらう
-
-
+    
     taskName=models.CharField(
-
         verbose_name= _("タスクの名前"),
         blank=False, max_length=50,)##50文字までの文字の入力が可能
-
+    
     deadline = models.DateTimeField(default=timezone.now,)##締切日 
-
     
+    importance=models.IntegerField(default=0,validators= [MinValueValidator(0), MaxValueValidator(2)]) ##重要度  0,1,2で重要度の大きさを決める。 validatorsで指定 
     
-    importance=models.IntegerField() ##重要度 
-    
-     
     note=models.TextField(
         verbose_name= _("メモ欄"),
         max_length=300,)
-
     
     isBocchi=models.BooleanField(verbose_name= _("ぼっち"),default=False)
 
-    img = models.ImageField(upload_to='',validators=[FileExtensionValidator(['jpg','png', ],imagefile_size)],verbose_name= _("画像"),)  ###画像ファイル
     
-    movie=models.FileField( upload_to='',verbose_name= _("動画"),validators=[FileExtensionValidator(['mp4','MPEG4','MOV', ],moviefile_size)],)
+    ###ここら辺の処理はまた違うときに作成される。
+    img = models.ImageField(upload_to='task',validators=[FileExtensionValidator(['jpg','png', ],imagefile_size)],verbose_name= _("画像"),)  ###画像ファイル
+    
+    movie=models.FileField( upload_to='task',verbose_name= _("動画"),validators=[FileExtensionValidator(['mp4','MPEG4','MOV', ],moviefile_size)],)
     #動画ファイル 動画の時間を制限する方法を探してみたけどなかなか見つからなかったのでまた探してみます。
-
-    
-    
     description=models.TextField( verbose_name= _("説明"),)
 
-    status=models.IntegerField()
+    status=models.IntegerField(default=0,validators= [MinValueValidator(0), MaxValueValidator(2)])
     
+    objects=TaskManager()##これを書くことでTaskManager()外部（vieews）でも使えるようになる。
 
-    
+class CommissionManager(models.Manager):
 
+    ## veiws.pyの処理をここに書く  関数を定義する・
     
+    def create(self,userid, taskid):    
+        ##コミッション版はTaskManagerがこの関数を呼び出す。
+        ###処理を分かりやすくするため。
+        ##データベースを作ってあげる。
+        
+        ##さっきの関数の引数でIsert INTO の処理を保存する。
+        
+        commission=Commission(userid=userid, taskid=taskid)
+        commission.save()
+            ##クリエイトをするための処理を書く
+
+
+class Commission(models.Model):
+    commissionid=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
+    taskid=models.ForeignKey(Task, on_delete=models.CASCADE)##タスクIDを外部からもらう
     
+    userid=models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    ###Commisionのデータベースを定義している。
+
+    objects = CommissionManager()
 
 
     
