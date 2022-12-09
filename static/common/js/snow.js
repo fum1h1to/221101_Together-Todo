e36778985be3a64ef04a137cd8088ee205a82f89
Fumihito Sudo <75571344+fum1h1to@users.noreply.github.com>
2022-12-10 01:03:37 +0900

    雪の枠組み作成

---
+++ static/common/js/snow.js
@@ -0,0 +1,15 @@
+(function () {
+// ↓ この中に処理を記述 ↓
+    function initSnow() {
+        /*
+            initSnowの最初のところは、下記の3行のコードじゃなくて、すでに書いてあるやつでよろしく。
+                const snow_div = document.createElement("div");
+                snow_div.id = "snow_div";
+                document.body.appendChild(snow_div); 
+        */
+        const snow_div = document.getElementById('js-snowField');
+
+    }
+
+// ↑ この中に処理を記述 ↑
+})();
